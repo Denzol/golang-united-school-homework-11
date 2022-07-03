5c829a08c4e1976b728c51d4f11a3727e2b77fa8
Hramtsov Denis <denis.khramtsov@mail.ru>
2022-07-03 20:56:27 +1000

    version 1(kastyl)

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,21 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	number := int(n)
+	parallel := int(pool)
+	var wg sync.WaitGroup
+	sem := make(chan struct{}, parallel)
+	for i := 0; i < number; i++ {
+		wg.Add(1)
+		sem <- struct{}{}
+		time.Sleep(time.Millisecond * 2)
+		go func(j int) {
+			user := getOne(int64(j))
+			res = append(res, user)
+			<-sem
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	return res
 }
